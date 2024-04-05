91447e0a15d90bea0206d56d1f4888bcd90e66f8
Mhayarax <brunjonathan@live.fr>
2024-04-05 09:30:42 +0200

    test

--- main.py
+++ main.py
@@ -5,26 +5,42 @@ app = Flask(__name__)
  
 @app.route("/nom")
 def return_nom():
-    return jsonify(Librairie.get_nom())
+    return jsonify(test.get_nom())
 
-test = Librairie("Libre et riz", "1 place du truc")
+test = Librairie("Libre et Riz","1 place de l'endroit")
+test.ajouterLivre("Le guide du zizi sexuel")
+test.ajouterLivre("Minecraft le livre")
+test.ajouterLivre("La Bible")
+ 
+@app.route('/librairie', methods=['GET'])
+def afficher_librairie():
+    librairie_info = {
+        'nom': test.get_nom(),
+        'adresse': test.get_adresse(),
+        'livres': test.get_livre()
+    }
+    return jsonify(librairie_info)
 
-# Ajouter des livres à la librairie
-lelivre = str(input("Entrez un livre à ajouter: "))
-test.ajouterLivre(lelivre)
+@app.route('/ajout', methods=['GET'])
+def ajouterLivre():
+    librairie_info = {
+        'nom': "test",
+    }
+    return jsonify(librairie_info)
 
-# Afficher les livres de la librairie
-livres = test.get_livre()
-print("Livres dans la librairie:")
-for livre in livres:
-    print("-", livre)
+@app.route('/librairie/livre', methods=['POST'])
+def ajouter_livre():
+    livre = request.json.get('livre')
+    test.ajouter_livre(livre)
+    return "Livre ajouté avec succès", 201
 
-# Ajouter des livres à la librairie
-lelivre = str(input("Entrez un livre à supprimer : "))
-test.supprimer_livre(lelivre)
+@app.route('/librairie/livre', methods=['DELETE'])
+def supprimer_livre():
+    livre = request.json.get('livre')
+    if test.supprimer_livre(livre):
+        return "Livre supprimé avec succès", 200
+    else:
+        return "Livre non trouvé dans la librairie", 404
 
-# Afficher les livres de la librairie
-livres = test.get_livre()
-print("Livres dans la librairie:")
-for livre in livres:
-    print("-", livre)
+if __name__ == '__main__':
+    app.run(debug=True)
